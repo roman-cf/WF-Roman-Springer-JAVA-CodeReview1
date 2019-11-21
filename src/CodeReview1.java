6be3b6d5b65648cf761fc8db9e26c66ac8cd5835
roman-cf <roman.springer@outlook.com>
2019-11-21 16:13:32 +0100

    starting

--- src/CodeReview1.java
+++ src/CodeReview1.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
 public class CodeReview1 {
     public static void main(String[] args) {
-
+    Scanner inputField = new Scanner(System.in);
+    System.out.print("Please enter the with and the height of an rectangular like this (8cm x 4cm): ");
+    String inputFieldStr = inputField.nextLine();
     }
 }
